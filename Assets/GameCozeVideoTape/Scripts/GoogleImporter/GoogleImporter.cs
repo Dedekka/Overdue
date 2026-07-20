c39891ec65b5c59c7e46ee5d45bba68860546e3c
Aliks175 <108829258+Aliks175@users.noreply.github.com>
2026-07-20 14:29:17 +0300

    Add_google

---
+++ Assets/GameCozeVideoTape/Scripts/GoogleImporter/GoogleImporter.cs
@@ -0,0 +1,19 @@
+using Google.Apis.Sheets.v4;
+using UnityEngine;
+
+public class GoogleImporter : MonoBehaviour
+{
+    
+    private readonly SheetsService sheetsService;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
